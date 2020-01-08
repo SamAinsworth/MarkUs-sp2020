a416a4e0dbb2164384ecb2b843b28aaf3d8e5034
Sam Ainsworth <sa614@cam.ac.uk>
2020-01-08 13:38:55 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 cd bdwgc-markus
 ./autogen.sh
-./configure --prefix=~/markus-allocator --enable-redirect-malloc --enable-threads=posix --disable-gc-assertions --enable-thread-local-alloc --enable-parallel-mark --disable-munmap --enable-cplusplus --enable-large-config --disable-gc-debug --enable-suspend-thread --disable-gcj-support --enable-sigrt-signals --enable-mmap
+./configure --prefix=$HOME/markus-allocator --enable-redirect-malloc --enable-threads=posix --disable-gc-assertions --enable-thread-local-alloc --enable-parallel-mark --disable-munmap --enable-cplusplus --enable-large-config --disable-gc-debug --enable-suspend-thread --disable-gcj-support --enable-sigrt-signals --enable-mmap
 make install
