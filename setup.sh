e05e1441f322bd9148f355a32cc7aac2f9da8a29
Sam Ainsworth <sa614@cam.ac.uk>
2022-01-03 16:45:58 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install autogen autoconf libtool
+sudo apt-get install autogen autoconf libtool make
 cd bdwgc-markus
 ./autogen.sh
 ./configure --prefix=$HOME/markus-allocator --enable-redirect-malloc --enable-threads=posix --disable-gc-assertions --enable-thread-local-alloc --enable-parallel-mark --disable-munmap --enable-cplusplus --enable-large-config --disable-gc-debug
